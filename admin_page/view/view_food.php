070835f0b5f722ddd2fc0e1c315626e9701ed3f9
Heart Wirunrom <heart569522@gmail.com>
2021-08-17 01:47:21 +0700

    add food

--- admin_page/view/view_food.php
+++ admin_page/view/view_food.php
@@ -2,11 +2,17 @@
 <div class="container-fluid">
 
 <!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">รายการอาหาร</h1>
-<!-- <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-    For more information about DataTables, please visit the <a target="_blank"
-        href="https://datatables.net">official DataTables documentation</a>.</p> -->
+<div class="row">
+    <div class="col-6">
+        <h1 class="h3 mb-2 text-gray-800">รายการอาหาร</h1>
+    </div>
+    <div class="col-6">
+        <a style="float: right;" href="#" class="btn btn-danger" >ลบเมนู</a>
+        <a style="float: right;" href="#" class="btn btn-success" >เพิ่มเมนู</a>
+    </div>
+</div>
 
+<br>
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <!-- <div class="card-header py-3">
