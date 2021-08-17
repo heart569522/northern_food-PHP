7ae5a637bb0f905c89f9e8080ea3980836e04649
Heart Wirunrom <heart569522@gmail.com>
2021-08-17 23:47:37 +0700

    form

--- add_food.php
+++ add_food.php
@@ -21,9 +21,31 @@
 
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
-            <div class="row">
-                <div class="col-6">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-6">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="">ชื่อ</label>
+                                <input type="text" class="form-control" name="food-name">
+                            </div>
+                            <div class="form-group">
+                                <label for="">ส่วนผสม</label>
+                                <textarea type="text" class="form-control" rows="5" name="ingredients"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="">เครื่องเคียง/เครื่องแกง</label>
+                                <textarea type="text" class="form-control" rows="5" name="spices"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="">รูป</label>
+                                <input type="file" class="form-control" id="formFile" name="food-img">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-6">
 
+                    </div>
                 </div>
             </div>
         </div>
