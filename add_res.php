28b15aa5923235f157562104250fc524cfe92276
Heart Wirunrom <heart569522@gmail.com>
2021-08-31 03:13:53 +0700

    Update add_res.php

--- add_res.php
+++ add_res.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once('./admin_page/model/database.php');
+include_once('./admin_page/model/connection.php');
 
 if ($_SESSION['admin_login'] == "") {
     header("location: signin.php");
@@ -32,37 +32,102 @@ if ($_SESSION['admin_login'] == "") {
     //         echo "<script>window.location.href='restaurant.php'</script>";
     //     }
     // }
-    try {
-        $r_name = $_REQUEST['res_name'];
-        $r_status = $_REQUEST['res-status'];
-        $r_map = $_REQUEST['res-map'];
+    if (isset($_REQUEST['insert_res'])) {
+        try {
+            $r_name = $_REQUEST['res-name'];
+            $r_status = $_REQUEST['res-status'];
+            $r_map = $_REQUEST['res-map'];
 
-        $img_file = $_FILES['res_img']['name'];
-        $img_type = $_FILES['res_img']['type'];
-        $img_size = $_FILES['res_img']['size'];
-        $img_temp = $_FILES['res_img']['tmp_name'];
-        $img_path = "upload/res/img" . $img_file;
+            $img_file = $_FILES['res-img']['name'];
+            $img_type = $_FILES['res-img']['type'];
+            $img_size = $_FILES['res-img']['size'];
+            $img_temp = $_FILES['res-img']['tmp_name'];
+            $img_path = "upload/res/img" . $img_file;
 
-        $bg_file = $_FILES['res_bg']['name'];
-        $bg_type = $_FILES['res_bg']['type'];
-        $bg_size = $_FILES['res_bg']['size'];
-        $bg_temp = $_FILES['res_bg']['tmp_name'];
-        $bg_path = "upload/res/bg" . $bg_file;
+            $bg_file = $_FILES['res-bg']['name'];
+            $bg_type = $_FILES['res-bg']['type'];
+            $bg_size = $_FILES['res-bg']['size'];
+            $bg_temp = $_FILES['res-bg']['tmp_name'];
+            $bg_path = "upload/res/bg" . $bg_file;
 
+            if (empty($r_name)) {
+                $errorMsg = "ใส่ชื่อร้านอาหาร";
+            } else if (empty($r_status)) {
+                $errorMsg = "เลือกสถานะของร้าน";
+            } else if (empty($r_map)) {
+                $errorMsg = "ใส่แผนที่";
+            } else if (empty($img_file)) {
+                $errorMsg = "เลือกรูปร้านอาหาร";
+            } else if (empty($bg_file)) {
+                $errorMsg = "เลือกรูปพื้นหลัง";
+            } else if ($img_type == "image/jpg" || $img_type == 'image/jpeg' || $img_type == "image/png" || $img_type == "image/gif") {
+                if (!file_exists($img_path)) { // check file not exist in your upload folder path
+                    if ($img_size < 5000000) { // check file size 5MB
+                        move_uploaded_file($img_temp, 'upload/res/img'.$img_file); // move upload file temperory directory to your upload folder
+                    } else {
+                        $errorMsg = "โปรดอัปโหลดไฟล์ที่มีขนาดไม่เกิน 5MB"; // error message file size larger than 5mb
+                    }
+                } else {
+                    $errorMsg = "มีไฟล์นี้อยู่แล้ว โปรดตรวจสอบในโฟลเดอร์ Upload"; // error message file not exists your upload folder path
+                }
+            } else if ($bg_type == "image/jpg" || $bg_type == 'image/jpeg' || $bg_type == "image/png" || $bg_type == "image/gif") {
+                if (!file_exists($bg_path)) { // check file not exist in your upload folder path
+                    if ($bg_size < 5000000) { // check file size 5MB
+                        move_uploaded_file($bg_temp, 'upload/res/bg'.$bg_file); // move upload file temperory directory to your upload folder
+                    } else {
+                        $errorMsg = "โปรดอัปโหลดไฟล์ที่มีขนาดไม่เกิน 5MB"; // error message file size larger than 5mb
+                    }
+                } else {
+                    $errorMsg = "มีไฟล์นี้อยู่แล้ว โปรดตรวจสอบในโฟลเดอร์ Upload"; // error message file not exists your upload folder path
+                }
+            } else {
+                $errorMsg = "โปรดอัปโหลดเฉพาะไฟล์ .JPG, .JPEG, .PNG, .GIF";
+            }
 
-    } catch(PDOException $e) {
-        $e->getMessage();
+            if (!isset($errorMsg)) {
+                $insert_stmt = $db->prepare('INSERT INTO nf_res(res_name, res_img, res_map, res_bg, res_status) VALUES (:r_name, :r_img, :r_map, :r_bg, :r_status)');
+                $insert_stmt->bindParam(':r_name', $r_name);
+                $insert_stmt->bindParam(':r_img', $img_file);
+                $insert_stmt->bindParam(':r_map', $r_map);
+                $insert_stmt->bindParam(':r_bg', $bg_file);
+                $insert_stmt->bindParam(':r_status', $r_status);
+
+                if ($insert_stmt->execute()) {
+                    $insertMsg = "เพิ่มข้อมูลสำเร็จ กรุณารอ...";
+                    header('refresh:1;');
+                    echo "<script>window.location.href = 'restaurant.php'</script>";
+                }
+            }
+
+        } catch(PDOException $e) {
+            $e->getMessage();
+        }
     }
 ?>
 
     <!-- Begin Page Content -->
     <div class="container-fluid">
-        <!-- Page Heading
-        <div class="row">
+        <!-- Page Heading -->
+        <!-- <div class="row">
             <div class="col-6">
                 <h1 class="h3 mb-2 text-gray-800">เพิ่มเมนู</h1>
             </div>
         </div> -->
+        <?php 
+            if(isset($errorMsg)) {
+        ?>
+            <div class="alert alert-danger">
+                <strong><?php echo $errorMsg; ?></strong>
+            </div>
+        <?php } ?>
+
+        <?php 
+            if(isset($insertMsg)) {
+        ?>
+            <div class="alert alert-success">
+                <strong><?php echo $insertMsg; ?></strong>
+            </div>
+        <?php } ?>
 
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
@@ -77,14 +142,14 @@ if ($_SESSION['admin_login'] == "") {
                             <div class="form-group">
                                 <label for="">สถานะร้านค้า</label>
                                 <select class="form-select" name="res-status">
-                                    <option disabled>Choose Status</option>
+                                    <option disabled>เลือกสถานะ</option>
                                     <option value="เปิด">เปิด</option>
                                     <option value="ปิด">ปิด</option>
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label class="form-label">รูปร้านอาหาร</label>
-                                <input class="form-control" type="file" name="res-img" accept="image/*" enctype="multipart/form-data">
+                                <input class="form-control" type="file" name="res-img">
                             </div>
                         </div>
                         <div class="col-12">
@@ -125,7 +190,7 @@ if ($_SESSION['admin_login'] == "") {
                         <div class="col-md-6 col-12">
                             <div class="form-group">
                                 <label class="form-label">รูปพื้นหลัง</label>
-                                <input class="form-control" type="file" name="res-bg" accept="image/*" enctype="multipart/form-data">
+                                <input class="form-control" type="file" name="res-bg">
                             </div>
                         </div>
                     </div>
